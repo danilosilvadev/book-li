6d8c18a43494ce41f1834bedb38cc24bbf249075
danilosilvadev <danilosilvaweb@hotmail.com>
2017-09-27 15:08:22 -0300

    issue3

--- src/app/components/MainRouter/MainRouter.js
+++ src/app/components/MainRouter/MainRouter.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './../../pages/home-page/home';
+import Reading from '../../pages/home-page/tabs/reading'
+import ToRead from '../../pages/home-page/tabs/to-read'
+import Readed from '../../pages/home-page/tabs/readed'
+import Dropped from '../../pages/home-page/tabs/dropped'
+
 
 class MainRouter extends Component {
   render() {
@@ -8,6 +13,10 @@ class MainRouter extends Component {
     <main>
         <Switch>
             <Route exact path='/' component={Home}/>
+            <Route path='/reading' component={Reading}/>
+                <Route path='/to-read' component={ToRead} />
+                <Route path='/readed' component={Readed} />
+                <Route path='/dropped' component={Dropped} />
         </Switch>
   </main>
     );
--- src/app/pages/home-page/menu.js
+++ src/app/pages/home-page/menu.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    NavLink
 } from 'react-router-dom'
 import Reading from './tabs/reading'
 import ToRead from './tabs/to-read'
@@ -10,61 +11,39 @@ import Readed from './tabs/readed'
 import Dropped from './tabs/dropped'
 import styled from 'styled-components';
 import MainPage from '../../components/MainPage/MainPage'
-import PropTypes from 'prop-types'; // ES6
-import { withRouter } from 'react-router';
 
 class Menu extends Component {
-    
+
     constructor(props) {
         super(props);
-        this.state = {
-            buttonColor: '#bfbfbf'
-        }
-        this.handleTab = this.handleTab.bind(this);        
+       
+        this.handleTab = this.handleTab.bind(this);
     }
-/*
+    state = {
+        buttonColor: '#bfbfbf'
+    }
+
     MenuStyle = {
         textDecoration: 'none',
         color: this.state.buttonColor
     }
 
-    static propTypes = {
-        match: PropTypes.object.isRequired,
-        location: PropTypes.object.isRequired,
-        history: PropTypes.object.isRequired
-      }
-
-    onBackButtonEvent(e) {
-        e.preventDefault();
-        this.context.propTypes.transitionTo('/');
-    }
-
-    componentDidMount() {
-        window.onpopstate = this.onBackButtonEvent;
-    }
-    */
-
 
     handleTab(e) {
-        e.preventDefault();        
+        e.preventDefault();
         this.setState({ buttonColor: 'red' });
     }
 
-   
-
-    
-
     render() {
-        console.log(this.context.history, this.context.location)        
         return <Router>
             <div>
                 <ul className='nav-list'>
-                    <li><Link to='/reading' className='nav' /*style={MenuStyle} */>Reading</Link></li>
+                    <li><NavLink to='/reading' className='nav' style={this.MenuStyle} isActive={this.handleTab}>Reading</NavLink></li>
                     <li><Link to='/to-read' className='nav' >To read</Link></li>
                     <li><Link to='/readed' className='nav' >Readed</Link></li>
                     <li><Link to='/dropped' className='nav' >Dropped</Link></li>
                 </ul>
-                <Route path='/reading' component={Reading} isActive={this.handleTab}/>
+                <Route path='/reading' component={Reading} />
                 <Route path='/to-read' component={ToRead} />
                 <Route path='/readed' component={Readed} />
                 <Route path='/dropped' component={Dropped} />
