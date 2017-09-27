0919f39b9dce0633cc3b68cb4da392a33e27d048
danilosilvadev <danilosilvaweb@hotmail.com>
2017-09-27 09:48:44 -0300

    issue3

--- src/app/pages/home-page/menu.js
+++ src/app/pages/home-page/menu.js
@@ -10,27 +10,39 @@ import Readed from './tabs/readed'
 import Dropped from './tabs/dropped'
 import styled from 'styled-components';
 import MainPage from '../../components/MainPage/MainPage'
-
+import PropTypes from 'prop-types'; // ES6
+import { withRouter } from 'react-router';
 
 class Menu extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
             buttonColor: '#bfbfbf'
         }
-        this.handleTab = this.handleTab.bind(this);
+        this.handleTab = this.handleTab.bind(this);        
+    }
+/*
+    MenuStyle = {
+        textDecoration: 'none',
+        color: this.state.buttonColor
     }
 
-   
+    static propTypes = {
+        match: PropTypes.object.isRequired,
+        location: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+      }
 
     onBackButtonEvent(e) {
         e.preventDefault();
-        return <MainPage />;
+        this.context.propTypes.transitionTo('/');
     }
 
     componentDidMount() {
         window.onpopstate = this.onBackButtonEvent;
     }
+    */
 
 
     handleTab(e) {
@@ -38,23 +50,21 @@ class Menu extends Component {
         this.setState({ buttonColor: 'red' });
     }
 
-    MenuStyle = {
-        textDecoration: 'none',
-        color: this.state.buttonColor
-    }
+   
 
     
 
     render() {
+        console.log(this.context.history, this.context.location)        
         return <Router>
             <div>
                 <ul className='nav-list'>
-                    <li><Link to='/reading' className='nav' style={MenuStyle} isActive={handleTab}>Reading</Link></li>
-                    <li><Link to='/to-read' className='nav' style={MenuStyle}>To read</Link></li>
-                    <li><Link to='/readed' className='nav' style={MenuStyle}>Readed</Link></li>
-                    <li><Link to='/dropped' className='nav' style={MenuStyle}>Dropped</Link></li>
+                    <li><Link to='/reading' className='nav' /*style={MenuStyle} */>Reading</Link></li>
+                    <li><Link to='/to-read' className='nav' >To read</Link></li>
+                    <li><Link to='/readed' className='nav' >Readed</Link></li>
+                    <li><Link to='/dropped' className='nav' >Dropped</Link></li>
                 </ul>
-                <Route path='/reading' component={Reading} />
+                <Route path='/reading' component={Reading} isActive={this.handleTab}/>
                 <Route path='/to-read' component={ToRead} />
                 <Route path='/readed' component={Readed} />
                 <Route path='/dropped' component={Dropped} />
